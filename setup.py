5bede3cbffc2c40932563d14a577faac73b4a3e1
ivannz <ivannz@yandex.ru>
2020-08-23 01:11:40 +0300

    setup script

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="charminGPTelegramBot",
+    version='0.1',
+    packages=[
+        'tbot',
+        'tbot.admin',
+        'tbot.control',
+        'tbot.utils',
+    ],
+    install_requires=[
+        'python-telegram-bot'
+    ]
+)
