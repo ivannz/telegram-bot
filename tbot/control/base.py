023120e7c07cf9ad1b1994b06269286761d202f2
ivannz <ivannz@yandex.ru>
2020-08-23 00:02:17 +0300

    base classes for the decorator-based access control

---
+++ tbot/control/base.py
@@ -0,0 +1,26 @@
+# no need for singleton as modules are imported only once
+class Singleton(type):
+    """https://stackoverflow.com/a/6798042"""
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class BaseDecorator:
+    def __init__(self):
+        self._context = {}
+
+    def __getitem__(self, function):
+        if isinstance(function, str):
+            function = dict(zip(dir(self), self)).get(function)
+
+        return self._context[function]
+
+    def __iter__(self):
+        return iter(self._context)
+
+    def __dir__(self):
+        return [f.__name__ for f in self]
